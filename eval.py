1a260cf4d8192c4fb0ed4e6e4a9a2d4d21e7b1cc
lhc <lhcezx@gmail.com>
2022-09-30 21:33:52 +0200

    update project

---
+++ eval.py
@@ -0,0 +1,44 @@
+import os
+import cv2
+import torch
+from piq import ssim
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+def getSSIM(im_dir):
+    images = sorted(os.listdir(im_dir), key = lambda x: int(x[:-4]))
+    SSIM = []
+    for f1, f2 in zip(images, images[1:]):
+        ref_im = cv2.imread(os.path.join(im_dir, f1))
+        ref_im = torch.from_numpy(ref_im).permute(2, 0, 1)[None, ...]
+        float_im = cv2.imread(os.path.join(im_dir, f2))
+        float_im = torch.from_numpy(float_im).permute(2, 0, 1)[None, ...]
+        
+        metric = ssim(float_im, ref_im, data_range = 255)            # Creat SSIM metric
+        SSIM.append(metric.item())
+    return SSIM
+
+
+if __name__ == "__main__":
+    # image path
+    im_dir_orig = 'images_janan'
+    im_dir_adam = 'images_painted'
+    im_dir_scaled = 'scaled_images'
+    x = np.arange(15)
+    # y1 = np.random.rand(220)
+    # y2 = np.random.rand(220)
+
+    # SSIM_orig = getSSIM(im_dir_orig)
+    # SSIM_adam = getSSIM(im_dir_adam)
+    SSIM_scaled = getSSIM(im_dir_scaled)
+
+    # dataframe = pd.DataFrame({'SSIM_original':SSIM_orig, 'SSIM_adam':SSIM_adam})
+    dataframe = pd.DataFrame({'SSIM_scaled':SSIM_scaled})
+    dataframe.to_csv("SSIM.csv", mode='a', index=False, sep=',')
+
+
+
+
+    
+
